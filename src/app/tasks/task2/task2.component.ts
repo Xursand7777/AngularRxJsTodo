1591f3bb22a7dad053cf88c42fa26fd893708f7b
Aibek <ai.bek@mail.ru>
2022-11-13 18:20:20 +0600

    feat: done task2

--- src/app/tasks/task2/task2.component.ts
+++ src/app/tasks/task2/task2.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Task2Service } from './task2.service';
 
 @Component({
   selector: 'app-task2',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Task2Component implements OnInit {
 
-  constructor() { }
+  randomNumbers$: Observable<number[]> = this.task2Service.randomNumber$;
+
+  numbersCount$: Observable<number> = this.task2Service.numbersCount$;
+
+  maxNumber$: Observable<number> = this.task2Service.maxNumber$;
+
+  minNumber$: Observable<number> = this.task2Service.minNumber$;
+
+  averageNumber$: Observable<number> = this.task2Service.averageNumber$;
+
+
+  constructor(private task2Service: Task2Service) { }
 
   ngOnInit(): void {
   }
 
+  addRandomNumber() {
+    this.task2Service.addRandomNumber();
+  }
 }
--- src/app/tasks/task2/task2.service.ts
+++ src/app/tasks/task2/task2.service.ts
@@ -1,27 +1,59 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, map, distinctUntilChanged, Observable } from 'rxjs';
 
 export interface IStore {
-  randomNumbers: number[]
+  randomNumbers: number[];
 }
 
-@Injectable()
+@Injectable({
+  providedIn: 'root',
+})
 export class Task2Service {
-
   private _store: BehaviorSubject<IStore> = new BehaviorSubject<IStore>({
-    randomNumbers: []
+    randomNumbers: [30, 3, 6],
   });
 
-  // public addRandomNumber(): void {
-  //
-  // }
-  //
-  // private _getRandomNumber(max: number = 100): number {
-  //
-  // }
-  //
-  // private _updateStore(data: Partial<IStore>): void {
-  //
-  // }
+  public randomNumber$ = this._store.pipe(
+    map((store) => store.randomNumbers),
+    distinctUntilChanged()
+  );
 
+  public numbersCount$: Observable<number> = this._store.pipe(
+    map((store) => store.randomNumbers.length),
+    distinctUntilChanged()
+  );
+
+  public maxNumber$: Observable<number> = this._store.pipe(
+    map((store) => Math.max(...store.randomNumbers)),
+    distinctUntilChanged()
+  );
+
+  public minNumber$: Observable<number> = this._store.pipe(
+    map((store) => Math.min(...store.randomNumbers)),
+    distinctUntilChanged()
+  );
+
+  public averageNumber$: Observable<number> = this._store.pipe(
+    map((store) =>
+      Math.round(
+        store.randomNumbers.reduce((a, b) => a + b) / store.randomNumbers.length
+      )
+    ),
+    distinctUntilChanged()
+  );
+
+  public addRandomNumber(): void {
+    const randomNumber = this._getRandomNumber();
+    this._updateStore({
+      randomNumbers: [...this._store.getValue().randomNumbers, randomNumber],
+    });
+  }
+
+  private _getRandomNumber(max: number = 100): number {
+    return Math.floor(Math.random() * max) + 1;
+  }
+
+  private _updateStore(data: Partial<IStore>): void {
+    this._store.next({ ...this._store.getValue(), ...data });
+  }
 }
